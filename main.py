002d1f68684d0d706d8c40f020b42d36916f4b07
Jeremiah-Barry Cailan <cailanjeremiahbarry@gmail.com>
2026-06-24 22:23:26 +0000

    added get_coordinates function

--- main.py
+++ main.py
@@ -2,10 +2,27 @@ import requests
 import os
 
 
-API_KEY = os.getenv()
+maps_key = os.getenv('MAPS_KEY')
+gemini_key = os.getenv('GEMINI_KEY')
+
 #converts users location into lat and long coordinates(for geminiapi)
 def get_coordinates(location):
-    url = "https://maps.googleapis.com/maps/api/geocode/"
+    #get the google maps api endpoint later
+    url = ("https://maps.googleapis.com/maps/api/geocode/json"f"?address={location}"f"&key={maps_key}")
+    response = requests.get(url)
+    data = response.json()
+
+    #error check if location does not exist
+    if data['status']!= "OK":
+        return None
+    
+    #get lat and long coordinates
+    coordinates = data['results'][0]["geometry"]["location"]
+    lattitude, longitude = coordinates['lat'],coordinates['lng']
+    return (lattitude,longitude)
+
+
+
 
 
     
