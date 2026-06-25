8839b34a67819f088c556bc86e270d30bfcbc956
Cailan Jeremiah-Barry <cailanjeremiahbarry@.com>
2026-06-25 15:56:42 -0400

    refactored api logic into api.py

--- api.py
+++ api.py
@@ -1,7 +1,13 @@
 import requests
+import json
 import os
 from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
+gemini_key = os.getenv('GEMINI_KEY')
+genai.api_key = gemini_key
+client = genai.Client(api_key=gemini_key)
 maps_key =  os.getenv('MAPS_KEY')
 
 
@@ -23,3 +29,29 @@ def get_coordinates(location):
     formatted_address = data["results"][0]["formatted_address"]
     lattitude, longitude = coordinates['lat'],coordinates['lng']
     return (formatted_address,lattitude,longitude)
+
+def create_activities(lat,long,budget):
+    response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    config=types.GenerateContentConfig(
+        system_instruction= "You are a travel tour guide recommending people activities." \
+        "Generate realistic activties near the provided coordinates. " \
+        "Only include activties that do not excede the user's budget" \
+        "Return only valid JSON, dont include any markdown, or explanations"),
+        contents = f"""Find at most 5 activites to do near:
+        Latitude: {lat}
+        Longitude: {long}
+        Budget limit: {budget}
+
+        Return JSON in the following format:
+        - for free activties make the cost 0. P
+
+        [{{"name": "Activity name",
+            "location": "Activity location"
+            "cost": "cost of the activty"}}
+        ]
+        
+        """
+    )
+    return json.loads(response.text)
+
